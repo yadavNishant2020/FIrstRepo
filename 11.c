f96b35b252f513904c010dc70a30dc9bca66b62c
Nishant Yadav <yadavnishant955@gmail.com>
2020-12-13 12:43:22 +0530

    initial commit

---
+++ 11.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+
+int age;
+
+
+    printf("Enter your age : ");
+    scanf("%d",&age);
+    (age>=18)?(printf("eligible foe vote")):(printf("not eligible for vote"));
+
+    return 0;
+}
+
